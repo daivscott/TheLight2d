85a160ef87d83bf85ca200c1ec3afb5f76ab6e56
daivscott <daivgit@mail.com>
2018-09-21 05:30:44 +0100

    Player movement added

--- public/js/game.js
+++ public/js/game.js
@@ -26,11 +26,14 @@ var game = new Phaser.Game(config);
 function preload() {
     // load the ship image
     this.load.image('ship', 'assets/spaceShips_001.png');
+    this.load.image('otherPlayer', 'assets/enemyBlack5.png');
 };
 
 function create() {
     var self = this;
     this.socket = io();
+    // create new group called otherPlayer
+    this.otherPlayers = this.physics.add.group();
     // callback to listen for currentPlayers
     this.socket.on('currentPlayers', function (players) {
         // loop through players and check id's match socket id
@@ -38,12 +41,50 @@ function create() {
             if (players[id].playerId === self.socket.id) {
                 // call addPlayer function and pass current player info
                 addPlayer(self, players[id]);
+            } else {
+                addOtherPlayers(self, players[id]);
             }
         });
     });
+    // listen for events
+    this.socket.on('newPlayer', function (playerInfo) {
+        addOtherPlayers(self, playerInfo);
+    });
+
+    // listen for disconnects
+    this.socket.on('disconnect', function (playerId) {
+        self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+            // destroy game objects that are disconnected
+            if (playerId === otherPlayer.playerId) {
+                otherPlayer.destroy();
+            }
+        });
+    });
+    // populate cursor object
+    this.cursors = this.input.keyboard.createCursorKeys();
 };
 
-function update() {};
+function update() {
+    if (this.ship) {
+        // check for forward movement input
+        if (this.cursors.left.isDown) {
+            this.ship.setAngularVelocity(-150);
+        } else if (this.cursors.right.isDown) {
+            this.ship.setAngularVelocity(150);
+        } else {
+            this.ship.setAngularVelocity(0);
+        }
+
+        // check for players rotational movement
+        if (this.cursors.up.isDown) {
+            this.physics.velocityFromRotation(this.ship.rotation + 1.5, 100, this.ship.body.acceleration);
+        } else {
+            this.ship.setAcceleration(0);
+        }
+        // allows the players to infinitely wrap across the screen
+        this.physics.world.wrap(this.ship, 5);
+    }
+};
 
 function addPlayer(self, playerInfo) {
     // create ship with arcade physics, set centred origin and scale
@@ -59,4 +100,15 @@ function addPlayer(self, playerInfo) {
     self.ship.setAngularDrag(100);
     // set max speed
     self.ship.setMaxVelocity(200);
+}
+
+function addOtherPlayers(self, playerInfo) {
+    const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'otherPlayer').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+    if (playerInfo.team === 'blue') {
+        otherPlayer.setTint(0x0000ff);
+    } else {
+        otherPlayer.setTint(0xff0000);
+    }
+    otherPlayer.playerId = playerInfo.playerId;
+    self.otherPlayers.add(otherPlayer);
 }
